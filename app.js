c70fea1502e8897693cad23943f85ea3bcd16dcd
Dharmesh Patel <d.patel5603@gmail.com>
2018-12-27 20:44:45 -0500

    set up routes to about, contact, resume and projects pages. Also set up partials for header and footer.

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const   express = require("express"),
         app     = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 app.set("view engine","ejs");
 
 app.get("/",function(req,res){
     res.render("landing");
 });
 
+app.get("/About",function(req,res){
+    res.render("about");
+});
+
+app.get("/Resume",function(req,res){
+    res.render("resume");
+});
+
+app.get("/Projects",function(req,res){
+    res.render("projects");
+});
+
+app.get("/Contact",function(req,res){
+    res.render("contact");
+});
+
+app.get("/*",function(req,res){
+    res.render("NotFound");
+});
 
 app.listen(3000, process.env.IP, function(){
     console.log("Running");
